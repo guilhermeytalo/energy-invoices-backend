9712de2d2caf0972c02810871c011d42c5b10778
Guilherme Ytalo <gydaytalo@gmail.com>
2023-12-22 10:39:41 -0300

    feat: fixed with prisma and connected with db

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello')
+import dotenv from 'dotenv';
+import express, { NextFunction, Request, Response } from 'express';
+
+dotenv.config();
+
+const app = express();
+
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
