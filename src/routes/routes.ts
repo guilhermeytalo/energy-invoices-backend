55575cc013b70471e9f9838a8a9c1b1c3b21c751
Guilherme Ytalo <gydaytalo@gmail.com>
2024-01-03 23:47:45 -0300

    feat: refactor to clean unused things

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,8 @@
-import express, { Request, Response } from 'express';
-import { getInvoices, uploadInvoice } from '../services/invoice.service';
-import { parsePDF } from '../controllers/invoiceController';
-import { getDashboardData } from '../services/dashboard.service';
-import multer from 'multer';
 import { PrismaClient } from '@prisma/client';
+import express, { Request, Response } from 'express';
+import multer from 'multer';
+import { getDashboardData } from '../services/dashboard.service';
+import { getInvoices, uploadInvoice } from '../services/invoice.service';
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -17,82 +16,9 @@ const upload = multer({
 
 router.get('/dashboard', getDashboardData);
 
-router.post(
-  '/upload',
-  upload.array('invoices'),
-  async (req: Request, res: Response) => {
-    try {
-      const files = req.files as Express.Multer.File[];
-      console.log('files', files);
+router.post('/upload', upload.array('invoices'), uploadInvoice);
 
-      const parsedDataPromises = files.map(
-        async (file) => await parsePDF(file.buffer, file.originalname)
-      );
-      console.log('parsedDataPromises', parsedDataPromises);
-
-      const parsedDataArray = await Promise.all(parsedDataPromises);
-      console.log('parsedDataArray', parsedDataArray);
-
-      const savedInvoicesPromises = parsedDataArray.map(async (data) => {
-        const eletricEnergy = await prisma.invoiceentry.create({
-          data: {
-            quant: data.text.energiaEletrica.quant,
-            unitprice: data.text.energiaEletrica.unitPrice,
-            value: data.text.energiaEletrica.value,
-            unittax: data.text.energiaEletrica.unitTax,
-          },
-        });
-        const sceeEnergy = await prisma.invoiceentry.create({
-          data: {
-            quant: data.text.sceeICMS.quant,
-            unitprice: data.text.sceeICMS.unitPrice,
-            value: data.text.sceeICMS.value,
-            unittax: data.text.sceeICMS.unitTax,
-          },
-        });
-        const compensedEnergy = await prisma.invoiceentry.create({
-          data: {
-            quant: data.text.energiaCompensada.quant,
-            unitprice: data.text.energiaCompensada.unitPrice,
-            value: data.text.energiaCompensada.value,
-            unittax: data.text.energiaCompensada.unitTax,
-          },
-        });
-
-        const invoiceData = {
-          clientnumber: data.text.cliente,
-          invoicemonthdate: data.text.referente,
-          eletricenergyid: eletricEnergy.id,
-          energysceeid: sceeEnergy.id,
-          compensedenergyid: compensedEnergy.id,
-          contributionilumination: data.text.contribIlumPublica,
-        };
-
-        await prisma.invoice.create({ data: invoiceData });
-
-        return 'Invoice created successfully'
-      });
-
-      console.log('savedInvoicesPromises', savedInvoicesPromises);
-      const savedInvoices = await Promise.all(savedInvoicesPromises);
-
-      res.status(200).json(savedInvoices);
-    } catch (error) {
-      console.error('Error parsing uploaded file:', error);
-      res.status(400).json({ error: 'Failed to parse uploaded file' });
-    }
-  }
-);
-
-router.get('/invoices', async (req: Request, res: Response) => {
-  try {
-    const invoices = await prisma.invoice.findMany();
-    res.status(200).json(invoices);
-  } catch (error) {
-    console.error('Error retrieving invoices:', error);
-    res.status(400).json({ error: 'Failed to retrieve invoices' });
-  }
-});
+router.get('/invoices', getInvoices);
 
 router.get('/invoices/:id', async (req: Request, res: Response) => {
   try {
--- src/services/invoice.service.ts
+++ src/services/invoice.service.ts
@@ -1,34 +1,80 @@
+import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
-import fs from 'fs';
 import { parsePDF } from '../controllers/invoiceController';
+
+const prisma = new PrismaClient();
 
 export const getInvoices = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   try {
-    const directoryPath = './src/assets/invoices';
-    const files = fs.readdirSync(directoryPath);
-
-    const parsedDataPromises = files.map(async (file) => {
-      const filePath = `${directoryPath}/${file}`;
-      return await parsePDF(filePath);
-    });
-
-    const parsedData = await Promise.all(parsedDataPromises);
-    res.status(200).json(parsedData);
+    const invoices = await prisma.invoice.findMany();
+    res.status(200).json(invoices);
   } catch (error) {
-    console.error('Error getting invoices:', error);
+    console.error('Error retrieving invoices:', error);
     res.status(400).json({ error: 'Failed to retrieve invoices' });
   }
 };
 
-export const uploadInvoice = async (req: Request, res: Response): Promise<void> => {
+export const uploadInvoice = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
-    const parsedData = await parsePDF(req?.file?.path ?? '');
-    res.status(200).json(parsedData);
+    const files = req.files as Express.Multer.File[];
+
+    const parsedDataPromises = files.map(
+      async (file) => await parsePDF(file.buffer, file.originalname)
+    );
+
+    const parsedDataArray = await Promise.all(parsedDataPromises);
+
+    const savedInvoicesPromises = parsedDataArray.map(async (data) => {
+      const eletricEnergy = await prisma.invoiceentry.create({
+        data: {
+          quant: data.text.energiaEletrica.quant,
+          unitprice: data.text.energiaEletrica.unitPrice,
+          value: data.text.energiaEletrica.value,
+          unittax: data.text.energiaEletrica.unitTax,
+        },
+      });
+      const sceeEnergy = await prisma.invoiceentry.create({
+        data: {
+          quant: data.text.sceeICMS.quant,
+          unitprice: data.text.sceeICMS.unitPrice,
+          value: data.text.sceeICMS.value,
+          unittax: data.text.sceeICMS.unitTax,
+        },
+      });
+      const compensedEnergy = await prisma.invoiceentry.create({
+        data: {
+          quant: data.text.energiaCompensada.quant,
+          unitprice: data.text.energiaCompensada.unitPrice,
+          value: data.text.energiaCompensada.value,
+          unittax: data.text.energiaCompensada.unitTax,
+        },
+      });
+
+      const invoiceData = {
+        clientnumber: data.text.cliente,
+        invoicemonthdate: data.text.referente,
+        eletricenergyid: eletricEnergy.id,
+        energysceeid: sceeEnergy.id,
+        compensedenergyid: compensedEnergy.id,
+        contributionilumination: data.text.contribIlumPublica,
+      };
+
+      await prisma.invoice.create({ data: invoiceData });
+
+      return 'Invoice created successfully';
+    });
+
+    const savedInvoices = await Promise.all(savedInvoicesPromises);
+
+    res.status(200).json(savedInvoices);
   } catch (error) {
     console.error('Error parsing uploaded file:', error);
     res.status(400).json({ error: 'Failed to parse uploaded file' });
   }
-}
+};
